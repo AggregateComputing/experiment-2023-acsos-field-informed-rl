13106b1de48cb498bd8bf2c92e7053f639d26171
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-05-16 13:06:00 +0200

    chore: improve the algorithm description

---
+++ src/main/scala/it/unibo/scafi/ExplorerCollectiveTwoLayerBasic.scala
@@ -0,0 +1,29 @@
+package it.unibo.scafi
+
+import it.unibo.learning.GaussianThreshold
+import it.unibo.scafi.space.Point3D
+
+class ExplorerCollectiveTwoLayer extends Agent {
+
+  override def main(): Any = {
+    val phenomena = senseEnvData[Double]("info")
+    val source = maxHood(nbr(phenomena)) == phenomena && phenomena > GaussianThreshold.Value
+    val direction = G[Point3D](source, Point3D.Zero, p => p + nbrVector(), nbrRange)
+    val data = includingSelf.reifyField(nbr(direction), nbr(phenomena), nbrRange(), nbr(currentPosition()))
+    node.put("hue", mid()) // for visualization
+    node.put("direction", direction)
+    node.put("directionArray", Array(direction.x, direction.y))
+    node.put("phenomena", phenomena)
+    node.put("source", source)
+    node.put("constant", 1.0)
+    node.put("inside", phenomena > GaussianThreshold.Value)
+    node.put("inside_1", senseEnvData[Double]("info1") > GaussianThreshold.Value)
+    node.put("inside_2", senseEnvData[Double]("info2") > GaussianThreshold.Value)
+    // compute angle from direction
+    node.put("center", math.atan2(-1 * direction.y, direction.x))
+    val state = computeState(data)
+    node.put("state", state)
+    // node.put("action", policy(state))
+  }
+
+}
