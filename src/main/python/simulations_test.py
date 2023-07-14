58283f04d3698c4cb4d95cf8e417ec4752be41f3
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-07-14 10:36:21 +0200

    feat: minors on simulation

--- src/main/python/simulations_test.py
+++ src/main/python/simulations_test.py
@@ -1,14 +1,14 @@
 import simulations_launcher
 
 simulation = [
-    #"simulationTestGNN",
-    #"simulationTestGNNMoving",
-    #"simulationTestGNNTwo",
-    #"simulationTestGNNBasic",
+    "simulationTestGNN",
+    "simulationTestGNNMoving",
+    "simulationTestGNNTwo",
+    "simulationTestGNNBasic",
     "simulationTestGNNMovingBasic",
-    #"simulationTestMLP",
-    #"simulationTestMLPTwo",
-    #"simulationTestMLPMoving"
+    "simulationTestMLP",
+    "simulationTestMLPTwo",
+    "simulationTestMLPMoving"
 ]
 
 simulations_launcher.run(64, simulation, 50)
